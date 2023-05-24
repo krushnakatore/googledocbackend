2d614cb9ed191dc6354c122e23973f3adc2c0686
Krushna Katore <91548932+krushnakatore@users.noreply.github.com>
2023-05-24 19:46:44 +0530

    Changed the url of frontend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = 9000;
 
 const io = new Server(PORT, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://googledox.vercel.app",
     methods: ["GET", "POST"],
   },
 });
